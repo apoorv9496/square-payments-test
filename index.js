6247616a3868eeaa133b2821dba00432f3009ff2
Apoorv Jain <apoorv9496@gmail.com>
2019-05-18 13:05:44 +0530

    port set to env allocated port

--- index.js
+++ index.js
@@ -112,6 +112,6 @@ app.post('/pay', async (request, response) => {
 });
 
 // listen for requests :)
-const listener = app.listen(5123, function() { //process.env.PORT
+const listener = app.listen(process.env.PORT, function() { //process.env.PORT
   console.log('Your app is listening on port ' + listener.address().port);
 });
